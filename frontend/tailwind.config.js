7f974bf248fddea8cc3ced2a73efb125a00c0954
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-31 15:10:02 -0600

    chore: add missing prettier fixes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-const tailwindTransforms = require("tailwindcss-transforms");
-const tailwindTransitions = require("tailwindcss-transitions");
+const tailwindTransforms = require('tailwindcss-transforms');
+const tailwindTransitions = require('tailwindcss-transitions');
 
 /* Helper Functions */
 // IMPORTANT: if we set the html font-size to a different value update the `base` here
@@ -21,7 +21,7 @@ const buildUtilityOptions = (start = 1, step = 4, limit = 64) => {
   const options = {};
 
   // Build values array handling start position and step
-  [...Array(limit / step).keys()].forEach((value) => {
+  [...Array(limit / step).keys()].forEach(value => {
     // Skip if below start
     if (start <= (value + 1) * step) {
       values.push(pxToRem((value + 1) * step));
@@ -38,43 +38,43 @@ const buildUtilityOptions = (start = 1, step = 4, limit = 64) => {
 
 /* Utility Config */
 const colors = {
-  transparent: "transparent",
-  black: "#000",
-  white: "#FFF",
-  "off-white": "#F7F7F7",
-  inherit: "inherit",
+  transparent: 'transparent',
+  black: '#000',
+  white: '#FFF',
+  'off-white': '#F7F7F7',
+  inherit: 'inherit',
   // Grey Shades
-  grey: "#828282",
-  "light-grey": "#F2F2F2",
+  grey: '#828282',
+  'light-grey': '#F2F2F2',
   // Theme Colors
-  orange: "#FF6D50",
+  orange: '#FF6D50',
 };
 
 const spacing = {
-  0: "0",
+  0: '0',
   ...buildUtilityOptions(1, 1, 256),
 };
 
 const sizing = {
-  auto: "auto",
+  auto: 'auto',
   ...buildUtilityOptions(1, 1, 200),
   ...buildUtilityOptions(210, 10, 1000),
 };
 
 const screens = {
-  xxs: "420px",
-  xs: "480px",
-  sm: "640px",
-  md: "768px",
-  xmd: "992px",
-  lg: "1024px",
-  xl: "1200px",
-  xxl: "1440px",
-  xxxl: "1600px",
+  xxs: '420px',
+  xs: '480px',
+  sm: '640px',
+  md: '768px',
+  xmd: '992px',
+  lg: '1024px',
+  xl: '1200px',
+  xxl: '1440px',
+  xxxl: '1600px',
 };
 
 const fontFamily = {
-  gelion: ["Gelion", "sans-serif"],
+  gelion: ['Gelion', 'sans-serif'],
 };
 
 const fontSize = {
@@ -111,40 +111,40 @@ const backgroundColor = {
 };
 
 const borderWidth = {
-  0: "0",
-  1: "1px",
+  0: '0',
+  1: '1px',
 };
 
 const borderColor = {
-  current: "currentColor",
+  current: 'currentColor',
   ...colors,
 };
 
 const borderRadius = {
-  full: "9999px",
+  full: '9999px',
 };
 
 const borderStyle = {
-  none: "none",
-  solid: "solid",
+  none: 'none',
+  solid: 'solid',
 };
 
 const width = {
-  "1/2": "50%",
-  "1/3": "33.33333%",
-  "2/3": "66.66667%",
-  "1/4": "25%",
-  "3/4": "75%",
-  "1/5": "20%",
-  "2/5": "40%",
-  "3/5": "60%",
-  "4/5": "80%",
-  "1/6": "16.66667%",
-  "5/6": "83.33333%",
-  fit: "fit-content",
-  full: "100%",
-  stretch: "calc(100% + 6rem)",
-  screen: "100vw",
+  '1/2': '50%',
+  '1/3': '33.33333%',
+  '2/3': '66.66667%',
+  '1/4': '25%',
+  '3/4': '75%',
+  '1/5': '20%',
+  '2/5': '40%',
+  '3/5': '60%',
+  '4/5': '80%',
+  '1/6': '16.66667%',
+  '5/6': '83.33333%',
+  fit: 'fit-content',
+  full: '100%',
+  stretch: 'calc(100% + 6rem)',
+  screen: '100vw',
   1200: pxToRem(1200),
   1400: pxToRem(1400),
   1440: pxToRem(1440),
@@ -154,139 +154,139 @@ const width = {
 };
 
 const height = {
-  full: "100%",
-  screen: "100vh",
-  "1/4": "25%",
-  "1/3": "33.33%",
-  "2/5": "40%",
-  "1/2": "50%",
-  "3/5": "60%",
-  "3/4": "75%",
+  full: '100%',
+  screen: '100vh',
+  '1/4': '25%',
+  '1/3': '33.33%',
+  '2/5': '40%',
+  '1/2': '50%',
+  '3/5': '60%',
+  '3/4': '75%',
   ...sizing,
 };
 
 const minWidth = {
-  0: "0",
-  none: "none",
+  0: '0',
+  none: 'none',
   ...width,
 };
 
 const minHeight = {
-  0: "0",
-  none: "none",
+  0: '0',
+  none: 'none',
   ...height,
 };
 
 const maxWidth = {
-  none: "none",
+  none: 'none',
   ...width,
 };
 
 const maxHeight = {
-  none: "none",
+  none: 'none',
   ...height,
 };
 
 const padding = { ...spacing };
 
 const negativeMargin = {
-  ...Object.keys(spacing).forEach((key) => {
+  ...Object.keys(spacing).forEach(key => {
     spacing[`-${key}`] = `-${spacing[key]}`;
   }),
 };
 
-const margin = { auto: "auto", "-1/10": "-10%", ...spacing, ...negativeMargin };
+const margin = { auto: 'auto', '-1/10': '-10%', ...spacing, ...negativeMargin };
 
 const boxShadow = {
-  bottom: "",
-  card: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
-  panel: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
-  pop: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
-  outline: "0 0 3px rgba(0,0,0,0.2)",
-  none: "none",
+  bottom: '',
+  card: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  panel: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+  pop: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+  outline: '0 0 3px rgba(0,0,0,0.2)',
+  none: 'none',
 };
 
 const negativeInset = {
-  ...Object.keys(spacing).forEach((key) => {
+  ...Object.keys(spacing).forEach(key => {
     spacing[`-${key}`] = `-${spacing[key]}`;
   }),
 };
 
 const inset = {
-  auto: "auto",
-  0: "0",
-  "-1": pxToRem(-1),
-  "-48": pxToRem(-48),
+  auto: 'auto',
+  0: '0',
+  '-1': pxToRem(-1),
+  '-48': pxToRem(-48),
   ...spacing,
   ...negativeInset,
 };
 
 const zIndex = {
-  auto: "auto",
-  "-1": "-1",
+  auto: 'auto',
+  '-1': '-1',
   0: 0,
-  1: "1",
-  100: "100",
+  1: '1',
+  100: '100',
 };
 
 const opacity = {
-  0: "0",
-  10: "0.10",
-  25: "0.25",
-  30: "30",
-  40: "40",
-  50: "0.5",
-  60: "60",
-  70: "70",
-  75: "0.75",
-  80: "80",
-  90: "90",
-  100: "1",
+  0: '0',
+  10: '0.10',
+  25: '0.25',
+  30: '30',
+  40: '40',
+  50: '0.5',
+  60: '60',
+  70: '70',
+  75: '0.75',
+  80: '80',
+  90: '90',
+  100: '1',
 };
 
 const outline = {
-  none: "none",
+  none: 'none',
   ...colors,
 };
 
 const fill = {
-  current: "currentColor",
+  current: 'currentColor',
   ...colors,
 };
 
 const stroke = {
-  current: "currentColor",
+  current: 'currentColor',
   ...colors,
 };
 
 const transform = {
-  none: "none",
+  none: 'none',
 };
 
 const translate = {
-  center: ["-50%", "-50%"],
+  center: ['-50%', '-50%'],
 };
 
 const scale = {
-  102: "1.02",
-  105: "1.05",
-  110: "1.1",
+  102: '1.02',
+  105: '1.05',
+  110: '1.1',
 };
 
 const flex = {
   1: 1,
   2: 2,
   3: 3,
-  none: "none",
+  none: 'none',
 };
 
 module.exports = {
-  prefix: "", // only edit if you absolutely must
+  prefix: '', // only edit if you absolutely must
   important: false, // not recommended to use important
-  separator: ":", // for variants
-  mode: "jit", // just-in-time Compiler
+  separator: ':', // for variants
+  mode: 'jit', // just-in-time Compiler
   media: false, // or 'darkMode' or 'class'
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
 
   /**
    * --------------------------------------------------------------------------
@@ -392,44 +392,44 @@ module.exports = {
     borderColor: [],
     borderRadius: [],
     borderWidth: [],
-    boxShadow: ["hover"],
-    display: ["responsive"],
+    boxShadow: ['hover'],
+    display: ['responsive'],
     fill: [],
-    flex: ["responsive"],
-    flexbox: ["responsive"],
-    flexDirection: ["responsive"],
-    flexGrow: ["responsive"],
-    flexShrink: ["responsive"],
-    flexWrap: ["responsive"],
+    flex: ['responsive'],
+    flexbox: ['responsive'],
+    flexDirection: ['responsive'],
+    flexGrow: ['responsive'],
+    flexShrink: ['responsive'],
+    flexWrap: ['responsive'],
     fontFamily: [],
-    fontSize: ["responsive"],
-    fontWeight: ["hover"],
-    height: ["responsive"],
-    inset: ["responsive"],
-    leading: ["responsive"],
-    margin: ["responsive"],
-    maxHeight: ["responsive"],
-    maxWidth: ["responsive"],
-    minHeight: ["responsive"],
-    minWidth: ["responsive"],
+    fontSize: ['responsive'],
+    fontWeight: ['hover'],
+    height: ['responsive'],
+    inset: ['responsive'],
+    leading: ['responsive'],
+    margin: ['responsive'],
+    maxHeight: ['responsive'],
+    maxWidth: ['responsive'],
+    minHeight: ['responsive'],
+    minWidth: ['responsive'],
     opacity: [],
-    outline: ["focus"],
-    overflow: ["responsive"],
-    padding: ["responsive", "first"],
-    position: ["responsive"],
-    scale: ["responsive", "hover"],
+    outline: ['focus'],
+    overflow: ['responsive'],
+    padding: ['responsive', 'first'],
+    position: ['responsive'],
+    scale: ['responsive', 'hover'],
     stroke: [],
-    textAlign: ["responsive"],
-    textColor: ["hover"],
-    transform: ["responsive"],
-    translate: ["responsive"],
-    transitionProperty: ["responsive"],
-    transitionDuration: ["responsive"],
-    transitionTimingFunction: ["responsive"],
-    transitionDelay: ["responsive"],
-    visibility: ["responsive"],
+    textAlign: ['responsive'],
+    textColor: ['hover'],
+    transform: ['responsive'],
+    translate: ['responsive'],
+    transitionProperty: ['responsive'],
+    transitionDuration: ['responsive'],
+    transitionTimingFunction: ['responsive'],
+    transitionDelay: ['responsive'],
+    visibility: ['responsive'],
     whitespace: [],
-    width: ["responsive"],
+    width: ['responsive'],
     zIndex: [],
   },
 
@@ -447,7 +447,7 @@ module.exports = {
   plugins: [
     tailwindTransforms({
       // https://github.com/benface/tailwindcss-transforms
-      "3d": false,
+      '3d': false,
       backfaceVisibility: false,
       perspective: false,
       perspectiveOrigin: false,
