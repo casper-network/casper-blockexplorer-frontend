82c92165f9a7e62c436959f408c4fbdfe13f252d
david <lostmorgan@gmail.com>
2022-09-02 13:04:58 -0400

    fixed button cross-browser  discrepancies

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -43,14 +43,14 @@ export const Header: React.FC = () => {
             onChange={ev => setSearch(ev.target.value)}
             type="search"
             id="search"
-            className="block p-4 pr-32 pl-10 w-400 text-sm text-gray-900 bg-gray-50 rounded-lg border-none focus:ring-blue-500 focus:border-blue-500 "
+            className="block p-4 pr-32 pl-10 w-400 text-sm text-gray-900 bg-gray-50 rounded-lg border-white border-opacity-0 focus:outline-none "
             placeholder="Public Key or Deploy Hash"
             required
           />
           <button
             onClick={submitValue}
             type="submit"
-            className="bg-[#FF0013] relative right-32 hover:bg-red-400 focus:ring-1  focus:ring-blue-300 font-medium rounded-lg ">
+            className="bg-[#FF0013] relative right-20 hover:bg-red-400 focus:outline-none  font-medium rounded-lg border-none">
             <svg
               aria-hidden="true"
               className="w-20 h-20 text-white pt-5"
@@ -58,6 +58,11 @@ export const Header: React.FC = () => {
               stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
               <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </button>
