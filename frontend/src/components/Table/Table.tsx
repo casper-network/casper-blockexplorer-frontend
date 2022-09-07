c541e1f8fd8ba15c7ddbfe31b1515ce259e26d7d
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-07 12:27:52 -0600

    refactor: table overflow-x; scroll below 800px

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -5,11 +5,11 @@ export const Table: React.FC<TableProps> = ({ headColumns, rows }) => {
   const totalRows = rows.length;
 
   return (
-    <div className="w-full my-32 shadow-card rounded-lg">
+    <div className="w-full my-32 shadow-card rounded-lg overflow-x-auto">
       <div className="w-full py-12">
         <p className="text-grey pl-32">{totalRows} total rows</p>
       </div>
-      <table className="table-auto w-full border-spacing-0">
+      <table className="table-auto w-full border-spacing-0 min-w-800">
         <thead className="bg-light-grey">
           <tr className="h-40">
             {headColumns.map(({ title, key }) => {
