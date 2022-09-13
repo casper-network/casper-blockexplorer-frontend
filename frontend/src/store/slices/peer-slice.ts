be32481c53492bd0463ddc43ea22099d02c96a42
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-12 22:11:41 -0600

    refactor: pull sdk data from casperApi class

--- frontend/src/store/slices/peer-slice.ts
+++ frontend/src/store/slices/peer-slice.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { getPeers } from '../../client';
+import { casperApi } from '../../api';
 import { Peer } from '../../types';
 import { Loading } from '../loading.type';
 
@@ -16,7 +16,7 @@ const initialState: PeerState = {
 
 export const fetchPeers = createAsyncThunk('rpcClient/fetchPeers', async () => {
   try {
-    const peers = await getPeers();
+    const peers = await casperApi.getPeers();
 
     return peers;
   } catch (error: any) {
