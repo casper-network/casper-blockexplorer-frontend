74c204793c5e1d59f080ae7a123de9cf7850dc38
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-12 23:36:17 -0600

    feat: setup router route catch & redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { StrictMode } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Header } from './components';
 import { AccountPage, BlockPage, DeployPage, Home, Peers } from './pages';
 
 const App = () => {
   return (
-    <div className="bg-light-grey grid min-h-screen grid-rows-layout">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/peers" element={<Peers />} />
-          <Route path="/account/:id" element={<AccountPage />} />
-          <Route path="/deploy/:id" element={<DeployPage />} />
-          <Route path="/block/:id" element={<BlockPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <StrictMode>
+      <div className="bg-light-grey grid min-h-screen grid-rows-layout">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/peers" element={<Peers />} />
+            <Route path="/account/:id" element={<AccountPage />} />
+            <Route path="/deploy/:id" element={<DeployPage />} />
+            <Route path="/block/:id" element={<BlockPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </StrictMode>
   );
 };
 
