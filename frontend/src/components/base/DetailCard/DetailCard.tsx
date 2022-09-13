11a1a1d90f674915e1c1a7514f4aba5e7c7e629b
david <lostmorgan@gmail.com>
2022-09-13 17:00:26 -0400

    Add hover effect to DetailCard component

--- frontend/src/components/base/DetailCard/DetailCard.tsx
+++ frontend/src/components/base/DetailCard/DetailCard.tsx
@@ -4,19 +4,21 @@ import { DetailCardProps } from './DetailCard.types';
 export const DetailCard: React.FC<DetailCardProps> = ({ rows }) => {
   return (
     <section className="w-full m-0 p-0">
-      <div className="w-full bg-[#FFF] shadow-card rounded-lg px-16 pb-0 overflow-x-auto">
+      <div className="w-full bg-[#FFF] shadow-card rounded-lg pb-0 overflow-x-auto">
         <table className="w-full">
           <tbody>
             {rows.map(({ detailKey, value, key }) => {
               return (
-                <tr key={key}>
-                  <td className="h-full py-16 border-0 border-b-1 border-light-grey border-solid text-slate-500 whitespace-nowrap pr-32">
-                    {detailKey}
-                  </td>
-                  <td className="h-full py-16 border-0 border-b-1 flex items-center border-light-grey border-solid">
-                    {value}
-                  </td>
-                </tr>
+                <div className="hover:bg-light-grey rounded-lg ">
+                  <tr key={key}>
+                    <td className="h-full py-16 border-0 border-b-1 border-light-grey border-solid text-slate-500 whitespace-nowrap pr-32 px-16">
+                      {detailKey}
+                    </td>
+                    <td className="h-full py-16 border-0 border-b-1 flex items-center border-light-grey border-solid">
+                      {value}
+                    </td>
+                  </tr>
+                </div>
               );
             })}
           </tbody>
