c9467856ce5b8247ea8e5812c07c8e2ebeca2e4d
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-13 00:12:07 -0600

    refactor: header styling

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -43,60 +43,62 @@ export const Header: React.FC = () => {
   };
 
   return (
-    <div>
-      <Navbar />
-      <div className="bg-casper-blue flex flex-row justify-center py-50 lg:absolute lg:left-0 lg:top-0 lg:pt-20 lg:pl-50">
-        <Link style={{ textDecoration: 'none' }} to="/">
-          <div className="flex flex-row">
-            <img className="h-35 xxs:h-50" src={logo} alt="Casper Logo" />
-            <h1 className="text-white text-21 xxs:text-24 xxs:pt-6 lg:pl-15 ">
-              Casper BlockExplorer
-            </h1>
-          </div>
-        </Link>
-      </div>
-      <form>
-        <label htmlFor="default-search" className="sr-only">
-          Search
-        </label>
-        <div className="bg-casper-blue flex relative justify-center  pl-10 pb-40 xmd:pl-30 lg:pt-20">
-          <select
-            onChange={ev => setFilter(ev.target.value)}
-            className="relative left-10 w-90 h-30 text-center rounded-r-none bg-casper-red rounded-lg border-none text-white focus:outline-none text-xs xxs:text-sm xxs:w-105">
-            <option value="account">Account</option>
-            <option value="deploy">Deploy Hash</option>
-            <option value="block">Block Hash</option>
-          </select>
-          <input
-            value={search}
-            onChange={ev => setSearch(ev.target.value)}
-            type="search"
-            id="search"
-            className="block p-4 pl-20 w-175 text-xs text-gray-900 bg-gray-50 rounded-lg  border-1 border-solid border-gray-400 focus:outline-none 
-             xxs:w-400 xxs:text-sm xxs:pr-32"
-            required
-          />
-          <button
-            onClick={submitValue}
-            type="submit"
-            className="bg-casper-red relative right-20 hover:bg-red-400 focus:outline-none  font-medium rounded-r-lg border-none">
-            <svg
-              aria-hidden="true"
-              className="w-20 h-20 text-white pt-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </button>
+    <header className="w-full bg-casper-blue flex justify-center">
+      <div className="relative w-full max-w-1600">
+        <Navbar />
+        <div className="flex flex-row justify-center py-50 lg:absolute lg:left-0 lg:top-0 lg:pt-20 lg:pl-50">
+          <Link style={{ textDecoration: 'none' }} to="/">
+            <div className="flex flex-row">
+              <img className="h-35 xxs:h-50" src={logo} alt="Casper Logo" />
+              <h1 className="text-white text-21 xxs:text-24 xxs:pt-6 lg:pl-15 ">
+                Casper BlockExplorer
+              </h1>
+            </div>
+          </Link>
         </div>
-      </form>
-    </div>
+        <form>
+          <label htmlFor="default-search" className="sr-only">
+            Search
+          </label>
+          <div className="bg-casper-blue flex relative justify-center pl-10 pb-40 xmd:pl-30 lg:pt-20">
+            <select
+              onChange={ev => setFilter(ev.target.value)}
+              className="relative left-10 w-90 h-32 sm:h-36 md:h-42 text-center rounded-r-none bg-casper-red rounded-lg border-none text-white focus:outline-none text-12 sm:text-14 md:text-16 xxs:w-105">
+              <option value="account">Account</option>
+              <option value="deploy">Deploy Hash</option>
+              <option value="block">Block Hash</option>
+            </select>
+            <input
+              value={search}
+              onChange={ev => setSearch(ev.target.value)}
+              type="search"
+              id="search"
+              className="block p-4 sm:p-6 md:p-10 pl-20 text-xs text-gray-900 bg-gray-50 rounded-lg border-1 border-solid border-gray-400 focus:outline-none 
+              xxs:w-400 xxs:text-sm xxs:pr-32"
+              required
+            />
+            <button
+              onClick={submitValue}
+              type="submit"
+              className="bg-casper-red relative right-20 px-16 hover:bg-red-400 focus:outline-none font-medium rounded-r-lg border-none">
+              <svg
+                aria-hidden="true"
+                className="w-24 h-24 text-white pt-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
+    </header>
   );
 };
