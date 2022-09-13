88c7e01f9f524f098e82f506c6634dca8dcf9933
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-12 23:26:51 -0600

    feat: create base page wrapper component

---
+++ frontend/src/components/layout/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Loader } from '../../utility/Loader';
+
+export interface PageError {
+  message: string;
+}
+
+interface PageWrapperProps {
+  readonly isLoading: boolean;
+  readonly error?: PageError;
+  readonly children: React.ReactNode;
+}
+
+interface BaseContentWrapperProps {
+  readonly children: React.ReactNode;
+}
+
+const BaseContentWrapper: React.FC<BaseContentWrapperProps> = ({
+  children,
+}) => {
+  return (
+    <main className="w-full max-w-1600 bg-light-grey py-24">
+      <div className="w-full h-full mx-auto px-24 md:px-32 xl:px-48">
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export const PageWrapper: React.FC<PageWrapperProps> = ({
+  isLoading,
+  error,
+  children,
+}) => {
+  if (error) {
+    return (
+      <BaseContentWrapper>
+        <h2 className="text-24 mb-8">Whoops! Something went wrong!</h2>
+        <p>{error.message}</p>
+      </BaseContentWrapper>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <BaseContentWrapper>
+        <Loader />
+      </BaseContentWrapper>
+    );
+  }
+
+  return <BaseContentWrapper>{children}</BaseContentWrapper>;
+};
--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,2 +1,3 @@
 export * from './Header';
 export * from './Navbar';
+export * from './PageWrapper';
