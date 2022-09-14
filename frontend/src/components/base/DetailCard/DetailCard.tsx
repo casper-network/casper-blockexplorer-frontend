0b6125278d62498e25e8755650ca25dded999b3c
david <lostmorgan@gmail.com>
2022-09-14 13:04:18 -0400

    remove hover effect from DetailsCard

--- frontend/src/components/base/DetailCard/DetailCard.tsx
+++ frontend/src/components/base/DetailCard/DetailCard.tsx
@@ -4,21 +4,19 @@ import { DetailCardProps } from './DetailCard.types';
 export const DetailCard: React.FC<DetailCardProps> = ({ rows }) => {
   return (
     <section className="w-full m-0 p-0">
-      <div className="w-full bg-[#FFF] shadow-card rounded-lg overflow-x-auto">
-        <table className="w-full border-collapse">
+      <div className="w-full bg-white shadow-card rounded-lg px-16 pb-0 overflow-x-auto">
+        <table className="w-full">
           <tbody>
             {rows.map(({ detailKey, value, key }) => {
               return (
-                <div className="hover:bg-light-grey">
-                  <tr key={key}>
-                    <td className="py-16 border-0 border-b-1 border-light-grey border-solid text-slate-500 whitespace-nowrap pr-32 px-16">
-                      {detailKey}
-                    </td>
-                    <td className="h-full py-16 border-0 border-b-1 flex items-center border-light-grey border-solid">
-                      {value}
-                    </td>
-                  </tr>
-                </div>
+                <tr key={key}>
+                  <td className="py-16 border-0 border-b-1 border-light-grey border-solid text-slate-500 whitespace-nowrap pr-32 px-16">
+                    {detailKey}
+                  </td>
+                  <td className="h-full py-16 border-0 border-b-1 flex items-center border-light-grey border-solid">
+                    {value}
+                  </td>
+                </tr>
               );
             })}
           </tbody>
