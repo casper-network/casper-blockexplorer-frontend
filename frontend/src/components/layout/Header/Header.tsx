3a6a794c7449e8399d0276895ac22ed38dce5360
david <lostmorgan@gmail.com>
2022-09-20 11:37:19 -0400

    Add comment

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -116,7 +116,7 @@ export const Header: React.FC = () => {
           <div className="bg-casper-blue flex relative justify-center pb-50 pt-4">
             <select
               {...register('filterOptions')}
-              className="relative left-10 w-90 h-32 sm:h-36 md:h-42 md:w-114 text-center rounded-r-none bg-casper-red rounded-lg border-none text-white focus:outline-none text-12 xs:text-13 sm:text-14 md:text-16 xxs:w-105">
+              className="relative left-10 w-90 h-32 sm:h-36 md:h-45 md:w-114 text-center rounded-r-none bg-casper-red rounded-lg border-none text-white focus:outline-none text-12 xs:text-13 sm:text-14 md:text-16 xxs:w-105">
               <option value="account">Account</option>
               <option value="deploy">Deploy Hash</option>
               <option value="block">Block Hash</option>
