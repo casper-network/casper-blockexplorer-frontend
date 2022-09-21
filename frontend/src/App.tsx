052828fc36a9b30d75678d7c81a638bfd787ec09
david <lostmorgan@gmail.com>
2022-09-21 14:52:00 -0400

    Update imports from components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,7 @@ import React, { StrictMode, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import useMeasure from 'react-use-measure';
 
-import { Header } from './components';
-import { Footer } from './components';
+import { Header, Footer } from './components';
 import { AccountPage, BlockPage, DeployPage, Home, Peers } from './pages';
 import { updateBounds, useAppDispatch } from './store';
 
