47874573dfeef924a2cf8e27a37f381c6ecedc9f
david <lostmorgan@gmail.com>
2022-09-21 15:23:55 -0400

    Remove typo

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="block bg-casper-blue text-white text-xs text-center pl py-25 ">
+    <footer className="block bg-casper-blue text-white text-xs text-center py-25">
       <p>Casper Node version: 1.4.8 (b94c4f79a)</p>
       <p>Network Name: `integration-test`</p>
     </footer>
