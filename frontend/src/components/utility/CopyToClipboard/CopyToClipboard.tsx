6e5f4601c50464dfc2fe7eddfbefc3a990885bb0
david <lostmorgan@gmail.com>
2022-09-22 21:22:32 -0400

    Update variable name and setTimeout interval

--- frontend/src/components/utility/CopyToClipboard/CopyToClipboard.tsx
+++ frontend/src/components/utility/CopyToClipboard/CopyToClipboard.tsx
@@ -16,11 +16,11 @@ export const CopyToClipboard: React.FC<CopyToClipboardProps> = ({
 
   useEffect(() => {
     if (isCopied) {
-      setTimeout(() => setCopied(false), 5000);
+      setTimeout(() => setCopied(false), 3000);
     }
   }, [isCopied]);
 
-  const copiedTextAndStyles = (
+  const copiedIcon = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -36,28 +36,28 @@ export const CopyToClipboard: React.FC<CopyToClipboardProps> = ({
     </svg>
   );
 
+  const copyIcon = (
+    <svg
+      width="17"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      className="fill-slate-500 hover:fill-casper-red focus:text-green-400 transition-all w-17 h-17 ml-5">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.5 2.5L13.5 2.5L13.5 9.5L6.5 9.5L6.5 2.5ZM5 4V1L15 1L15 11H12V14L2 14L2 4L5 4ZM5 5.5H3.5L3.5 12.5L10.5 12.5V11L5 11L5 5.5Z"
+      />
+    </svg>
+  );
+
   return (
     <button
       className="border-none bg-transparent"
       type="button"
       disabled={isCopied}
       onClick={copyFn}>
-      {isCopied ? (
-        copiedTextAndStyles
-      ) : (
-        <svg
-          width="17"
-          height="16"
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-slate-500 hover:fill-casper-red focus:text-green-400 transition-all w-17 h-17 ml-5">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M6.5 2.5L13.5 2.5L13.5 9.5L6.5 9.5L6.5 2.5ZM5 4V1L15 1L15 11H12V14L2 14L2 4L5 4ZM5 5.5H3.5L3.5 12.5L10.5 12.5V11L5 11L5 5.5Z"
-          />
-        </svg>
-      )}
+      {isCopied ? copiedIcon : copyIcon}
     </button>
   );
 };
