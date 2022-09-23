e22887e785c11099fa24e0aae4326284267fb540
david <lostmorgan@gmail.com>
2022-09-23 11:40:26 -0400

    Button svg to component

--- frontend/src/components/layout/DemoHeader/DemoHeader.tsx
+++ frontend/src/components/layout/DemoHeader/DemoHeader.tsx
@@ -7,6 +7,7 @@ import logo from '../../../assets/images/logo.png';
 import { ReactComponent as OpenMenuIcon } from '../../../assets/icons/open-menu-icon.svg';
 import { ReactComponent as CloseMenuIcon } from '../../../assets/icons/close-menu-icon.svg';
 import { ReactComponent as ErrorIcon } from '../../../assets/icons/error-icon.svg';
+import { ReactComponent as ButtonIcon } from '../../../assets/icons/button-icon.svg';
 
 type FormValues = {
   hash: string;
@@ -160,20 +161,7 @@ export const DemoHeader: React.FC = () => {
           <button
             type="submit"
             className="bg-casper-red relative right-20 px-16 md:mt-7 hover:bg-red-400 focus:outline-none font-medium rounded-r-lg border-none">
-            <svg
-              aria-hidden="true"
-              className="w-24 h-24 text-white pt-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+            <ButtonIcon />
           </button>
         </div>
         {errors.hash && (
