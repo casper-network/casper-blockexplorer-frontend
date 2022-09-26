40e3e7e324f85a96a18b744188a76dcb5de6309d
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2022-09-26 08:22:45 -0700

    add mobile carousel to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAsyncEffect from 'use-async-effect';
 
+import { MobileBlocksCarousel } from 'src/components/cards/MobileBlocksDisply/MobileBlocksCarousel';
 import { BlockTable, PageWrapper } from '../components';
 
 import {
@@ -10,6 +11,7 @@ import {
   useAppSelector,
   Loading,
   fetchBlocks,
+  getBounds,
 } from '../store';
 
 export const Home: React.FC = () => {
@@ -19,6 +21,18 @@ export const Home: React.FC = () => {
   const blockLoadingStatus = useAppSelector(getBlockLoadingStatus);
 
   const isLoading = blockLoadingStatus !== Loading.Complete;
+  const [isMobile, setIsMobile] = useState(false);
+
+  const bounds = useAppSelector(getBounds);
+
+  const windowWidth = bounds?.width || 0;
+
+  if (!isMobile && windowWidth < 1024) {
+    setIsMobile(true);
+  }
+  if (isMobile && windowWidth > 1024) {
+    setIsMobile(false);
+  }
 
   useAsyncEffect(async () => {
     if (blockLoadingStatus === Loading.Idle) {
@@ -29,7 +43,11 @@ export const Home: React.FC = () => {
   return (
     <PageWrapper isLoading={isLoading}>
       <h2 className="text-24 mb-25">Blocks</h2>
-      <BlockTable blocks={blocks} />
+      {isMobile ? (
+        <MobileBlocksCarousel blocks={blocks} />
+      ) : (
+        <BlockTable blocks={blocks} />
+      )}
     </PageWrapper>
   );
 };
