cde821ee9ed21be672c98d00c3dc9b69a47ee56a
Jan Hoffmann <jan@hfmn.pl>
2022-09-27 20:01:00 +0200

    refactor: redux selector added in footer

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../../store';
+import { useAppSelector, getNetworkStatus } from '../../../store';
 
 export const Footer: React.FC = () => {
-  const { version, build, networkName } = useSelector(
-    (state: RootState) => state.networkStatus,
-  );
+  const { version, build, networkName } = useAppSelector(getNetworkStatus);
 
   return (
     <footer className="block bg-casper-blue text-white text-xs text-center py-25">
