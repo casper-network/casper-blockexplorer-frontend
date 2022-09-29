952965eeeb104550eae4e9b002d1e3c12945d092
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-09-29 12:33:56 -0600

    feat: test base detail card component

--- frontend/src/components/base/DetailCard/DetailCard.tsx
+++ frontend/src/components/base/DetailCard/DetailCard.tsx
@@ -7,9 +7,9 @@ export const DetailCard: React.FC<DetailCardProps> = ({ rows }) => {
       <div className="w-full bg-white shadow-card rounded-lg px-16 overflow-x-auto max-w-screen-p-incl py-0">
         <table className="w-full">
           <tbody>
-            {rows.map(({ detailKey, value, key }) => {
+            {rows.map(({ detailKey, value, key }, index) => {
               return (
-                <tr key={key}>
+                <tr key={key} data-testid={index + 1}>
                   <td className="h-full w-200 py-16 border-0 border-b-1 border-light-grey border-solid text-slate-500 whitespace-nowrap pr-32 align-top">
                     {detailKey}
                   </td>
