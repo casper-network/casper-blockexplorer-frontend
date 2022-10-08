4101f9b5db4b0243651e8f87c8b9a6be97b00a25
david <lostmorgan@gmail.com>
2022-10-08 19:21:01 -0400

    Update background color for wrapper

--- frontend/src/components/layout/PageWrapper/PageWrapper.tsx
+++ frontend/src/components/layout/PageWrapper/PageWrapper.tsx
@@ -21,7 +21,10 @@ const BaseContentWrapper: React.FC<BaseContentWrapperProps> = ({
   children,
 }) => {
   return (
-    <main className="w-full max-w-1600 mx-auto bg-light-grey py-24">
+    // <main className="w-full max-w-1800 mx-auto bg-[] py-24">
+    //   <div className="w-full h-full px-24 md:px-32 xl:px-48">{children}</div>
+    // </main>
+    <main className="w-full max-w-1800 mx-auto bg-[#fff] py-24">
       <div className="w-full h-full px-24 md:px-32 xl:px-48">{children}</div>
     </main>
   );
