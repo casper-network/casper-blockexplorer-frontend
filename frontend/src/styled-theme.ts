d46aa81f15c5587e8ba7038d44cc168f744bf3b4
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-10-10 12:57:27 -0600

    feat: add breakpoints to styled sudo theme

--- frontend/src/styled-theme.ts
+++ frontend/src/styled-theme.ts
@@ -17,6 +17,18 @@ export const colors = {
   green: '#09A129',
 };
 
+export const breakpoints = {
+  xxs: '420px',
+  xs: '480px',
+  sm: '640px',
+  md: '768px',
+  xmd: '992px',
+  lg: '1024px',
+  xl: '1200px',
+  xxl: '1440px',
+  xxxl: '1600px',
+};
+
 export const pxToRem = (px: number, base: number = 16) => {
   return `${px / base}rem`;
 };
