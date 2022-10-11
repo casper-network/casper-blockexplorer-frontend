15b3a834df1057b478009c3ebaa63b9d3aed7438
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-10-10 23:40:23 -0600

    feat: setup single file to export icons

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as ButtonIcon } from '../../assets/icons/button-icon.svg';
+export { ReactComponent as CloseMenuIcon } from '../../assets/icons/close-menu-icon.svg';
+export { ReactComponent as FailureIcon } from '../../assets/icons/failure.svg';
+export { ReactComponent as OpenMenuIcon } from '../../assets/icons/open-menu-icon.svg';
+export { ReactComponent as SuccessIcon } from '../../assets/icons/success.svg';
