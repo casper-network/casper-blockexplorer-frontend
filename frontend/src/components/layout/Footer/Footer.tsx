6e6e444c6eec7cdd931959e2d778ea1298bda4e2
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-10-12 14:10:16 -0600

    refactor: pull org logo and name from ENV

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useAppSelector, getNetworkStatus } from '../../../store';
 
+const orgName = process.env.ORG_NAME || 'Casper';
+
 export const Footer: React.FC = () => {
   const { version, build, networkName } = useAppSelector(getNetworkStatus);
 
   return (
     <footer className="block bg-casper-blue text-white text-xs text-center py-25">
       <p>
-        Casper Node version: {version} ({build})
+        {orgName} Node version: {version} ({build})
       </p>
       <p>Network Name: {networkName}</p>
     </footer>
