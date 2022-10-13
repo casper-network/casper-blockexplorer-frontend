d3a6ccd24d60f7e60b61c4a49b14cedefe3b2084
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-10-12 18:39:39 -0600

    feat: create load config utility for typed config

---
+++ frontend/src/utils/load-config.ts
@@ -0,0 +1,22 @@
+export const loadConfig = () => {
+  const {
+    NODE_ENV,
+    REACT_APP_MIDDLEWARE_URL: middlewareUrl,
+    REACT_APP_ORG_NAME: orgName = 'Casper',
+    REACT_APP_ORG_LOGO_URL: orgLogoUrl,
+  } = process.env;
+
+  const isProduction = NODE_ENV === 'production';
+
+  // temporary; remove after sidecar integration
+  if (!middlewareUrl) {
+    throw new Error('Invalid Config: Missing MIDDLEWARE_URL');
+  }
+
+  return {
+    isProduction,
+    middlewareUrl,
+    orgName,
+    orgLogoUrl,
+  };
+};
