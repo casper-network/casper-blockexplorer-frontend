17c02c811dd731430bc4d63e93fd48767f9e801b
david <lostmorgan@gmail.com>
2022-11-24 13:00:10 -0500

    refactor: create NavButton component

---
+++ frontend/src/components/buttons/NavButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { breakpoints } from 'src/styled-theme';
+import { Button } from '../base';
+import { ButtonProps } from '../base/Button/Button';
+
+export const NavButton: React.FC<ButtonProps> = ({
+  children,
+  type,
+  onClick,
+  color,
+}) => {
+  return (
+    <NavButtonContainer>
+      <StyledButton>
+        <Button type={type} onClick={onClick} color={color}>
+          {children}
+        </Button>
+      </StyledButton>
+    </NavButtonContainer>
+  );
+};
+
+export const NavButtonContainer = styled.div`
+  z-index: 30;
+`;
+
+export const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  margin: 0 auto;
+  padding: 0;
+  width: 90%;
+  border-style: none;
+
+  @media (min-width: ${breakpoints.lg}) {
+    display: none;
+  }
+`;
