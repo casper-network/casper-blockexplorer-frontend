e001dd1670abf14ce47fc3f67349c5d58d951f17
david <lostmorgan@gmail.com>
2022-12-07 11:04:03 -0500

    refactor: button test

--- frontend/src/components/base/Button/Button.test.tsx
+++ frontend/src/components/base/Button/Button.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { fireEvent, render } from '../../../test-utils';
+import userEvent from '@testing-library/user-event';
+import { render } from '../../../test-utils';
 import { Button } from './Button';
 
 describe('Button', () => {
+  const mockCallBack = jest.fn();
+
   it('should render without errors', () => {
     const { getByRole } = render(<Button type="button">button</Button>);
     const button = getByRole('button', { name: 'button' });
@@ -11,24 +14,30 @@ describe('Button', () => {
   });
 
   it('should produce an event on click if not disabled', () => {
-    const { getByRole } = render(<Button type="button">button</Button>);
-    const button = getByRole('button', { name: 'button' });
-
-    fireEvent.click(button);
-
-    expect(button).toHaveTextContent('button');
-    expect(button).toBeEnabled();
-  });
-
-  it('should not produce an event on click if it contains disabled prop', () => {
     const { getByRole } = render(
-      <Button type="button" isDisabled>
+      <Button type="button" onClick={mockCallBack}>
         button
       </Button>,
     );
     const button = getByRole('button', { name: 'button' });
 
-    fireEvent.click(button);
+    userEvent.click(button);
+    expect(button).toBeEnabled();
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+    expect(button).toHaveTextContent('button');
+  });
+
+  it('should not produce an event on click if it contains disabled prop', () => {
+    const { getByRole } = render(
+      <Button type="button" onClick={mockCallBack} isDisabled>
+        button
+      </Button>,
+    );
+    const button = getByRole('button', { name: 'button' });
+
+    userEvent.click(button);
     expect(button).not.toBeEnabled();
+    expect(mockCallBack.mock.calls.length).toEqual(0);
+    expect(button).toHaveTextContent('button');
   });
 });
