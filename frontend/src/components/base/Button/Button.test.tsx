f1163746be81ff4daaccb77eaa934685637f68d0
david <lostmorgan@gmail.com>
2022-12-07 11:37:27 -0500

    refactor: button test description and mockFn

--- frontend/src/components/base/Button/Button.test.tsx
+++ frontend/src/components/base/Button/Button.test.tsx
@@ -4,7 +4,7 @@ import { render } from '../../../test-utils';
 import { Button } from './Button';
 
 describe('Button', () => {
-  const mockCallBack = jest.fn();
+  const mockClickHandler = jest.fn();
 
   it('should render', () => {
     const { getByRole } = render(<Button type="button">button</Button>);
@@ -13,9 +13,9 @@ describe('Button', () => {
     expect(button).toBeInTheDocument();
   });
 
-  it('should produce an event on click if not disabled', () => {
+  it('should call a clickHandler if not disabled', () => {
     const { getByRole } = render(
-      <Button type="button" onClick={mockCallBack}>
+      <Button type="button" onClick={mockClickHandler}>
         button
       </Button>,
     );
@@ -23,13 +23,13 @@ describe('Button', () => {
 
     userEvent.click(button);
     expect(button).toBeEnabled();
-    expect(mockCallBack.mock.calls.length).toEqual(1);
+    expect(mockClickHandler.mock.calls.length).toEqual(1);
     expect(button).toHaveTextContent('button');
   });
 
-  it('should not produce an event on click if it contains disabled prop', () => {
+  it('should not call a clickHandler if it contains disabled prop', () => {
     const { getByRole } = render(
-      <Button type="button" onClick={mockCallBack} isDisabled>
+      <Button type="button" onClick={mockClickHandler} isDisabled>
         button
       </Button>,
     );
@@ -37,7 +37,7 @@ describe('Button', () => {
 
     userEvent.click(button);
     expect(button).not.toBeEnabled();
-    expect(mockCallBack.mock.calls.length).toEqual(0);
+    expect(mockClickHandler.mock.calls.length).toEqual(0);
     expect(button).toHaveTextContent('button');
   });
 });
