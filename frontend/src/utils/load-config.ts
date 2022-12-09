e5412f1e8952a966f732575ccce290aa38b3c6af
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-12-09 14:21:07 -0700

    refactor: add support for nginx env variable injection

--- frontend/src/utils/load-config.ts
+++ frontend/src/utils/load-config.ts
@@ -1,12 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 export interface AppConfig {
   isProduction: boolean;
   webServerUrl: string;
 }
 
+/* eslint-disable prefer-destructuring */
+const ENV = (window as any).ENV;
+Object.keys(ENV).forEach(key => {
+  ENV[key] = ENV[key].indexOf('<!--') !== -1 ? null : ENV[key];
+});
+
 export const loadConfig: () => AppConfig = () => {
-  const { NODE_ENV, REACT_APP_MIDDLEWARE_URL: webServerUrl } = process.env;
+  const { NODE_ENV, REACT_APP_MIDDLEWARE_URL: reactAppMiddlewareUrl } =
+    process.env;
+  const { MIDDLEWARE_URL: middlewareUrl } = ENV;
 
   const isProduction = NODE_ENV === 'production';
+
+  const webServerUrl = isProduction ? middlewareUrl : reactAppMiddlewareUrl;
 
   if (!webServerUrl) {
     throw new Error('Invalid Config: Missing MIDDLEWARE_URL');
