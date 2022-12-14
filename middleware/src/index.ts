8d6b591898242a2b53e361c4d321a31289b82874
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-12-15 06:53:43 +0900

    fix(eslint): import order

--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -1,5 +1,5 @@
-import { PORT } from "./config";
 import app from "./app";
+import { PORT } from "./config";
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
