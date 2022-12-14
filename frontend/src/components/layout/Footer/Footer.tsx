1ffbc0a66c93acb36784b26ee6a49a0050404b70
david <lostmorgan@gmail.com>
2022-12-14 18:18:09 -0500

    chore: updated path

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { colors, fontWeight } from 'src/styled-theme';
+import { colors, fontWeight } from '../../../styled-theme';
 import { useAppSelector, getNetworkStatus } from '../../../store';
 
 export const Footer: React.FC = () => {
