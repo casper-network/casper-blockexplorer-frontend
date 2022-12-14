94cf998505c5e606e61a73492027cae6a5d1bb7e
david <lostmorgan@gmail.com>
2022-12-14 13:54:55 -0500

    styles: update loader padding

--- frontend/src/components/utility/Loader/Loader.tsx
+++ frontend/src/components/utility/Loader/Loader.tsx
@@ -25,14 +25,14 @@ export const Loader: React.FC<LoaderProps> = ({ size } = { size: 'lg' }) => {
   }
 
   return (
-    <div className="h-full w-full grid place-items-center">
+    <div className="h-full w-full grid place-items-center pb-150">
       <div className={loaderSizeClasses} aria-label="Loading..." role="status">
         <svg
           data-testid="loader"
           className={`${loaderSizeClasses} animate-spin`}
           viewBox="3 3 18 18">
           <path
-            className="fill-[#0421CB]"
+            className="fill-[#0421CB] pb-1000s"
             d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
           />
           <path
