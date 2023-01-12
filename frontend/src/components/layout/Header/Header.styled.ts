34cf1ddece4237cd319c36239e9e4050f781ffea
david <lostmorgan@gmail.com>
2023-01-12 17:43:59 -0500

    styles: update Header/Hero Styles

--- frontend/src/components/layout/Header/Header.styled.ts
+++ frontend/src/components/layout/Header/Header.styled.ts
@@ -15,7 +15,7 @@ export const HeaderComponentsContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
-  padding: 1.75rem 1.7rem 0rem 2.17rem;
+  padding: 1.75rem 1.7rem 1.7rem 2.17rem;
 
   @media (min-width: ${breakpoints.lg}) {
     justify-content: space-between;
@@ -76,11 +76,13 @@ export const ExplorerLogo = styled(ExpLogo)`
 export const HeroContainer = styled.div<{ isFirstVisit: boolean }>`
   display: ${({ isFirstVisit }) => (isFirstVisit ? 'flex' : 'none')};
   justify-content: center;
-  padding-top: 2rem;
+  padding: 0;
 
-  @media (min-width: ${breakpoints.lg}) {
+  @media (min-width: ${breakpoints.md}) {
+    padding-top: 1rem;
     justify-content: start;
     width: 64.5%;
+    min-width: ${pxToRem(628)};
     max-width: ${pxToRem(792)};
     margin: 0 auto;
   }
@@ -96,17 +98,8 @@ export const HeroHeading = styled(GradientHeading)`
   @media (min-width: ${breakpoints.md}) {
     font-size: 3.2rem;
     line-height: 1;
-    padding-right: 3rem;
-    max-width: 34rem;
+    padding-right: 0rem;
+    max-width: 31rem;
     background-size: 100%;
-  }
-
-  @media (min-width: ${breakpoints.lg}) {
-    font-size: 3.2rem;
-    line-height: 1;
-    text-align: left;
-    max-width: 42.5rem;
-    padding-right: 11rem;
-    background-size: 72.5%;
   }
 `;
