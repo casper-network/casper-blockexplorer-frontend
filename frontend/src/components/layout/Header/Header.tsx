da1660c545b31a552bf03a092db00720261eb547
david <lostmorgan@gmail.com>
2023-01-13 20:13:36 -0500

    refactor: update Header components

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -19,27 +19,51 @@ import {
 
 export const Header: React.FC = () => {
   const { t } = useTranslation();
-  const { isDropdownMenu } = useAppWidth();
+  const { isDropdownMenu, isMobile } = useAppWidth();
 
   const isFirstVisit = useAppSelector(getIsFirstVisit);
 
   return (
-    <HeaderComponent>
-      <HeaderComponentsContainer>
-        <LogoLink to="/">
-          <BlueBlackCasperLogo />
-          <BlueCasperLogo />
-          <ExplorerLogo />
-        </LogoLink>
-        <Navbar />
-      </HeaderComponentsContainer>
-      {isDropdownMenu ? <SearchForm /> : null}
-      <HeroContainer isFirstVisit={isFirstVisit}>
-        <HeroHeading type="h1" aria-label="Casper Block Explorer">
-          {t('discover-casper')}
-        </HeroHeading>
-      </HeroContainer>
-      {!isDropdownMenu ? <SearchForm /> : null}
-    </HeaderComponent>
+    <div>
+      {isFirstVisit ? (
+        <HeaderComponent>
+          <HeaderComponentsContainer isFirstVisit={isFirstVisit}>
+            <LogoLink to="/" isFirstVisit={isFirstVisit}>
+              <BlueBlackCasperLogo />
+              <BlueCasperLogo />
+              <ExplorerLogo />
+            </LogoLink>
+            <Navbar />
+          </HeaderComponentsContainer>
+          {isDropdownMenu && <SearchForm />}
+          <HeroContainer isFirstVisit={isFirstVisit}>
+            <HeroHeading type="h1" aria-label="Casper Block Explorer">
+              {t('discover-casper')}
+            </HeroHeading>
+          </HeroContainer>
+          {!isDropdownMenu && <SearchForm />}
+        </HeaderComponent>
+      ) : (
+        <HeaderComponent>
+          {!isMobile ? (
+            <HeaderComponentsContainer isFirstVisit={isFirstVisit}>
+              <Navbar />
+            </HeaderComponentsContainer>
+          ) : (
+            <div>
+              <HeaderComponentsContainer isFirstVisit={isFirstVisit}>
+                <LogoLink to="/" isFirstVisit={isFirstVisit}>
+                  <BlueBlackCasperLogo />
+                  <BlueCasperLogo />
+                  <ExplorerLogo />
+                </LogoLink>
+                <Navbar />
+              </HeaderComponentsContainer>
+              <SearchForm />
+            </div>
+          )}
+        </HeaderComponent>
+      )}
+    </div>
   );
 };
