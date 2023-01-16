db345f39055ebba7bb6adadbd120926f273b9752
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-01-16 17:59:01 +0900

    chore: update node env

--- middleware/src/config.ts
+++ middleware/src/config.ts
@@ -8,10 +8,14 @@ const REQUIRED_ENVS = [
   "SIDE_CAR_EVENTSTREAM_URL",
 ];
 
-REQUIRED_ENVS.forEach((requiredEnv) => {
-  if (!Object.keys(process.env).includes(requiredEnv))
-    throw Error(`Missing ${requiredEnv} env variable`);
-});
+export const NODE_ENV = process.env.NODE_ENV || "production";
+
+if (NODE_ENV !== "test") {
+  REQUIRED_ENVS.forEach((requiredEnv) => {
+    if (!Object.keys(process.env).includes(requiredEnv))
+      throw Error(`Missing ${requiredEnv} env variable`);
+  });
+}
 
 export const PORT = process.env.PORT || 4000;
 export const NODE_URLS = process.env.NODE_URLS;
@@ -19,4 +23,3 @@ export const SIDE_CAR_REST_URL = process.env.SIDE_CAR_REST_URL as string;
 export const SIDE_CAR_EVENTSTREAM_URL = process.env
   .SIDE_CAR_EVENTSTREAM_URL as string;
 export const PEERS_UPDATE_INTERVAL = process.env.PEERS_UPDATE_INTERVAL || 120;
-export const NODE_ENV = process.env.NODE_ENV || "production";
--- middleware/src/env.d.ts
+++ middleware/src/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NODE_ENV: "development" | "production";
+    NODE_ENV: "development" | "production" | "test";
     PORT?: number;
     MIDDLEWARE_URL: string;
     NODE_URLS: string;
