fbc40db1fc5f86d4be974888e6e93e200dd64d55
david <lostmorgan@gmail.com>
2023-01-16 16:08:46 -0500

    refactor: move header logos styles to logo components

--- frontend/src/components/layout/Header/Header.styled.ts
+++ frontend/src/components/layout/Header/Header.styled.ts
@@ -1,9 +1,6 @@
-import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { GradientHeading } from '../../styled';
 import { breakpoints, pxToRem } from '../../../styled-theme';
-
-import { BlueBlackLogo, BlueLogo, ExpLogo } from '../../logos';
 
 export const HeaderComponent = styled.header`
   width: 100%;
@@ -21,53 +18,6 @@ export const HeaderComponentsContainer = styled.div<{ isFirstVisit: boolean }>`
     padding: 3.5rem 7% 1.75rem 7%;
     transform: 3.5rem 5.3rem 1.75rem 5.3rem;
   }
-`;
-
-export const LogoLink = styled(Link)<{ isFirstVisit: boolean }>`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  text-decoration-line: none;
-  width: 100%;
-
-  :hover,
-  :focus {
-    text-decoration-line: none;
-  }
-
-  @media (min-width: ${breakpoints.xxs}) {
-    width: 9rem;
-  }
-
-  @media (min-width: ${breakpoints.lg}) {
-    padding: 0;
-    width: ${({ isFirstVisit }) => (isFirstVisit ? '15rem' : '9rem')};
-  } ;
-`;
-
-export const BlueCasperLogo = styled(BlueLogo)`
-  width: 20%;
-  margin-right: 5px;
-
-  @media (min-width: ${breakpoints.lg}) {
-    display: none;
-  }
-`;
-
-export const BlueBlackCasperLogo = styled(BlueBlackLogo)`
-  display: none;
-  width: 20%;
-
-  @media (min-width: ${breakpoints.lg}) {
-    display: block;
-    width: 90%;
-  }
-`;
-
-export const ExplorerLogo = styled(ExpLogo)`
-  display: block;
-  width: 80%;
 `;
 
 export const HeroContainer = styled.div<{ isFirstVisit: boolean }>`
