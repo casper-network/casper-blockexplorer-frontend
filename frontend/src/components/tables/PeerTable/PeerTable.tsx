4b32a76b983b026d434be5c60d9fcb5c549640df
Jan Hoffmann <jan@hfmn.pl>
2023-01-27 16:03:14 +0100

    fix: removed peers details on peers page

--- frontend/src/components/tables/PeerTable/PeerTable.tsx
+++ frontend/src/components/tables/PeerTable/PeerTable.tsx
@@ -1,12 +1,10 @@
 import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import styled from '@emotion/styled';
 import { ColumnDef } from '@tanstack/react-table';
 import { colors, fontWeight } from 'src/styled-theme';
 import { Peer } from '../../../api';
 import { Table } from '../../base';
-import { truncateHash } from '../../../utils';
 
 interface PeerTableProps {
   readonly peers: Peer[];
@@ -25,36 +23,6 @@ export const PeerTable: React.FC<PeerTableProps> = ({ peers }) => {
         header: `${t('address')}`,
         accessorKey: 'address',
         enableSorting: false,
-      },
-      {
-        header: `${t('uptime')}`,
-        accessorKey: 'uptime',
-        enableSorting: false,
-      },
-      {
-        header: `${t('last-block-hash')}`,
-        accessorKey: 'lastAddedBlockHash',
-        enableSorting: false,
-        cell: ({ getValue }) => (
-          <div className="flex flex-row items-center">
-            <Link
-              to={{
-                pathname: `/block/${getValue<string>()}`,
-              }}>
-              {truncateHash(getValue<string>())}
-            </Link>
-          </div>
-        ),
-      },
-      {
-        header: `${t('is-alive')}`,
-        accessorKey: 'isAlive',
-        enableSorting: false,
-        cell: ({ getValue }) => (
-          <div className="flex flex-row items-center">
-            {getValue<boolean>() ? t('up') : t('down')}
-          </div>
-        ),
       },
     ],
     [t],
