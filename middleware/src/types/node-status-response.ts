2dc7d77644a9d026f7f10cd9e406ae933d3bb969
Jan Hoffmann <jan@hfmn.pl>
2023-01-27 16:00:14 +0100

    refactor: disabled problematic peers checks

--- middleware/src/types/node-status-response.ts
+++ middleware/src/types/node-status-response.ts
@@ -7,7 +7,4 @@ export interface StatusResponse {
 export interface Peer {
   nodeId: string;
   address: string;
-  isAlive: boolean | null;
-  uptime: string | null;
-  lastAddedBlockHash: string | null;
 }
