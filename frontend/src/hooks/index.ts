b56677df17da7ef5e0ad1beada16a1e1c7b3c5de
mgibson-casper <matthew@casperlabs.io>
2023-02-10 12:55:09 -0700

    refactor-176: remove remaining old blocks and useBlocks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,4 +1,2 @@
 export { useAppRefresh } from './use-app-refresh';
 export { useAppWidth } from './use-app-width';
-export { default as useBlocks } from './useBlocks';
-export * from './useBlocks';
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,5 @@
 export { AccountPage } from './Account';
 export { BlockPage } from './Block';
-export { Blocks as BlocksOld } from './BlocksOld';
 export { Blocks } from './Blocks';
 export { DeployPage } from './Deploy';
 export { Home } from './Home';
