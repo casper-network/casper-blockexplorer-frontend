eb718267b8f16e86f31fda8414bb5f387b989938
david <lostmorgan@gmail.com>
2023-02-24 13:36:26 -0500

    styles: refactor ConfigurableLogo to handle .env size

--- app/src/components/layout/LogoComponents/ConfigurableLogo.tsx
+++ app/src/components/layout/LogoComponents/ConfigurableLogo.tsx
@@ -15,51 +15,31 @@ export const ConfigurableLogo: React.FC = () => {
   const envWidth = logoSize > 100 ? 100 : logoSize;
 
   return (
-    <ConfigurableLogoWrapper
+    <ConfigurableLogoContainer
       envWidth={envWidth}
       isMobile={isMobile}
       isFirstVisit={isFirstVisit}>
       <Link to="/">
         <ConfigLogo src={logoUrl} alt="configuration logo" />
       </Link>
-    </ConfigurableLogoWrapper>
+    </ConfigurableLogoContainer>
   );
 };
 
-export const ConfigurableLogoWrapper = styled.div<{
+export const ConfigurableLogoContainer = styled.div<{
   isMobile: boolean;
   isFirstVisit: boolean;
   envWidth: number;
 }>`
-  /* border: solid 1px hotpink; */
-
-  /* REFACTOR  */
-
   width: ${({ envWidth }) => envWidth}%;
   min-width: ${({ isFirstVisit, isMobile }) =>
-    isFirstVisit || isMobile ? `${pxToRem(200)}` : `${pxToRem(200)}`};
+    isFirstVisit || isMobile ? `${pxToRem(0)}` : `${pxToRem(0)}`};
   max-width: ${({ isFirstVisit, isMobile }) =>
-    isFirstVisit || isMobile ? `${pxToRem(320)}` : `${pxToRem(500)}`};
+    isFirstVisit || isMobile ? `${pxToRem(345)}` : `${pxToRem(500)}`};
   padding: ${({ isMobile }) =>
     isMobile
       ? `${pxToRem(20)} 0 ${pxToRem(20)} ${pxToRem(10)}`
       : `${pxToRem(15)} 0`};
-  /* padding:  ${({ isFirstVisit, isMobile }) =>
-    isFirstVisit || isMobile ? `${pxToRem(320)}` : `${pxToRem(500)}`}; */
-
-  /* ORIGINAL  */
-  /*
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  text-decoration-line: none;
-   contain: content;
-   min-width: ${pxToRem(200)};
-  max-height: ${({ isMobile }) =>
-    isMobile ? `${pxToRem(55)}` : `${pxToRem(45)}`};
-  max-width: ${({ isMobile }) =>
-    isMobile ? `${pxToRem(250)}` : `${pxToRem(200)}`};
-  padding-top: ${({ isMobile }) => (isMobile ? `${pxToRem(7)}` : '0')}; */
 
   :hover,
   :focus {
