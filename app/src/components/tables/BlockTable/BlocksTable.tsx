1666581767718f22b70f8a73586a72e3ce384bfb
mgibson-casper <matthew@casperlabs.io>
2023-02-24 08:17:26 -0700

    feat-194: revert back to const outside of component

--- app/src/components/tables/BlockTable/BlocksTable.tsx
+++ app/src/components/tables/BlockTable/BlocksTable.tsx
@@ -23,6 +23,12 @@ import { CopyToClipboard, RefreshTimer } from '../../utility';
 import { Table } from '../../base';
 import { NumberedPagination } from '../Pagination/NumberedPagination';
 
+const rowCountSelectOptions: SelectOptions[] | null = [
+  { value: '5', label: '5 rows' },
+  { value: '10', label: '10 rows' },
+  { value: '20', label: '20 rows' },
+];
+
 interface BlocksTableProps {
   readonly total?: number;
   readonly blocks: ApiData.Block[];
@@ -54,31 +60,6 @@ export const BlocksTable: React.FC<BlocksTableProps> = ({
   const totalPages = useMemo(() => {
     return Math.ceil(totalBlocks / blocksTableOptions.pagination.pageSize);
   }, [blocksTableOptions, totalBlocks]);
-
-  const rowCountSelectOptions: SelectOptions[] | null = useMemo(
-    () => [
-      {
-        value: '5',
-        label: t('rows', {
-          count: 5,
-        }),
-      },
-      {
-        value: '10',
-        label: t('rows', {
-          count: 10,
-        }),
-      },
-      {
-        value: '20',
-        label: t('rows', {
-          count: 20,
-        }),
-      },
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [t],
-  );
 
   useEffect(() => {
     const interval = setInterval(() => {
