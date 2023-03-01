5411ff08fd9171e2f7e9134da55c3495c43dfa73
david <lostmorgan@gmail.com>
2023-03-01 18:23:15 -0500

    refactor: simplify Navar.test

--- app/src/components/layout/Navbar/Navbar.test.tsx
+++ app/src/components/layout/Navbar/Navbar.test.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import { render } from '../../../test-utils';
 import { Navbar } from './Navbar';
 
+jest.mock('../../../hooks', () => {
+  return {
+    useAppWidth: () => {
+      return { windowWidth: 1022, isMobile: false };
+    },
+  };
+});
+
+jest.mock('react-i18next', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const englishTranslations = jest.requireActual(
+    '../../../../public/locales/en/translation.json',
+  );
+
+  return {
+    useTranslation: () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+      return { t: (key: string) => englishTranslations[key] };
+    },
+  };
+});
+
 describe('Navbar', () => {
   it('should render the Navbar component and contain nav items', () => {
     const { getByTestId } = render(<Navbar />);
+
     const nav = getByTestId('navigation');
 
     expect(nav).toBeInTheDocument();
