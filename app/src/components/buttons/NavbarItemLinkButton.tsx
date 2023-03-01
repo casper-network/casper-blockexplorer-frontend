633f296e5e33d6a868c1a3e68f79077d4ba9866e
david <lostmorgan@gmail.com>
2023-03-01 15:45:57 -0500

    feat: create NavbarItemLinkButton component

---
+++ app/src/components/buttons/NavbarItemLinkButton.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Button } from 'src/components/base';
+import { pxToRem, colors } from 'src/styled-theme';
+
+export interface NavbarItemLinkButtonProps {
+  readonly title: string;
+  readonly children: React.ReactNode;
+
+  // TODO:
+  readonly selectedRoute: any;
+  // TODO:
+  readonly handleNavItemSelection: (event: {
+    target: {
+      innerText: React.SetStateAction<null>;
+    };
+  }) => void;
+}
+
+export const NavbarItemLinkButton: React.FC<NavbarItemLinkButtonProps> = ({
+  handleNavItemSelection,
+  selectedRoute,
+  title,
+  children,
+}) => (
+  <DesktopNavItemLink
+    type="button"
+    onClick={handleNavItemSelection}
+    selectedRoute={selectedRoute}
+    title={title}>
+    {children}
+  </DesktopNavItemLink>
+);
+const DesktopNavItemLink = styled(Button)<{
+  selectedRoute: string;
+  title: string;
+}>`
+  color: ${({ selectedRoute, title }) =>
+    selectedRoute === `${title}` ? `${colors.white}` : `${colors.primary}`};
+  background-color: ${({ selectedRoute, title }) =>
+    selectedRoute === `${title}` ? `${colors.primary}` : 'transparent'};
+  transition: all 0.2s ease;
+  padding: ${pxToRem(6)} ${pxToRem(20)};
+  font-size: clamp(0.9rem, 1.2vw, 1.4rem);
+  font-weight: 500;
+  text-decoration: none;
+  border-radius: ${pxToRem(8)};
+
+  :focus {
+    text-decoration: none;
+  }
+
+  :active,
+  :hover {
+    color: ${({ selectedRoute, title }) =>
+      selectedRoute === `${title}` ? `${colors.white}` : `${colors.primary}`};
+    background-color: ${({ selectedRoute, title }) =>
+      selectedRoute === `${title}`
+        ? `${colors.primary}`
+        : `${colors.secondary}`};
+    padding: ${pxToRem(6)} ${pxToRem(20)};
+    text-decoration: none;
+  }
+`;
