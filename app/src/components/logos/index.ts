80eaf36f72b2d74be0fd04618d484459b71a1685
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-04-14 11:55:28 -0700

    fix: remove legacy icon files

--- app/src/components/logos/index.ts
+++ app/src/components/logos/index.ts
@@ -1,5 +1,1 @@
-export { ReactComponent as BlueLogo } from '../../assets/images/blue-casper-logo.svg';
-export { ReactComponent as WhiteLogo } from '../../assets/images/white-casper-logo.svg';
 export { ReactComponent as RedBlackLogo } from '../../assets/images/red-black-casper-logo.svg';
-export { ReactComponent as BlkExpLogo } from '../../assets/images/block-explorer-gradient-logo.svg';
-export { ReactComponent as ExpLogo } from '../../assets/images/explorer-gradient-logo.svg';
