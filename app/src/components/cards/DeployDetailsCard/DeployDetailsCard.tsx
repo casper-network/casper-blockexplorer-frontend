4509b21122a9e87340e76031379bb6aae7ce3e29
mgibson-casper <matthew@casperlabs.io>
2023-04-22 09:08:19 -0600

    feat-263: update some styles for deploy details

--- app/src/components/cards/DeployDetailsCard/DeployDetailsCard.tsx
+++ app/src/components/cards/DeployDetailsCard/DeployDetailsCard.tsx
@@ -6,9 +6,8 @@ import { useAppWidth } from 'src/hooks';
 import { HashButton } from 'src/components/buttons';
 import { hashPlaceholder } from 'src/utils';
 import { Deploy } from '../../../api';
-import { Heading, InfoCard, HeadContentWrapper } from '../../base';
+import { Heading, InfoCard } from '../../base';
 import {
-  GradientHeading,
   Hash,
   DetailDataLabel,
   DetailDataWrapper,
@@ -28,14 +27,14 @@ export const DeployDetailsCard: React.FC<DeployDetailsCardProps> = ({
   deploy,
   isLoading,
 }) => {
-  const [isTruncated, setIsTruncated] = useState<boolean>(true);
+  const [isTruncated, setIsTruncated] = useState(true);
   const { isMobile } = useAppWidth();
   const { t } = useTranslation();
 
   return (
-    <InfoCard>
-      <HeadContentWrapper>
-        <DeployHeading type="h1">{t('deploy-details')}</DeployHeading>
+    <>
+      <HeaderContent>
+        {/* TODO: should add a "back to" here (and in all other details pages) */}
         <HashWrapper>
           {withSkeletonLoading(
             <>
@@ -58,73 +57,75 @@ export const DeployDetailsCard: React.FC<DeployDetailsCardProps> = ({
             { width: 350, height: 60 },
           )}
         </HashWrapper>
-      </HeadContentWrapper>
-      <DetailDataWrapper>
-        <DetailDataList>
-          <li>
-            <DetailDataLabel>{t('block-hash')}</DetailDataLabel>
-            <DetailDataValue height="2rem">
-              {withSkeletonLoading(
-                <>
-                  <Link to={`/block/${deploy?.blockHash ?? ''}`}>
-                    <Hash hash={deploy?.blockHash ?? hashPlaceholder} />
-                  </Link>
-                  <CopyToClipboard textToCopy={deploy?.blockHash ?? ''} />
-                </>,
-                isLoading,
-                { width: '60%' },
-              )}
-            </DetailDataValue>
-          </li>
-          <li>
-            <DetailDataLabel>{t('public-key')}</DetailDataLabel>
-            <DetailDataValue height="2rem">
-              {withSkeletonLoading(
-                <>
-                  <Link to={`/account/${deploy?.publicKey ?? ''}`}>
-                    <Hash hash={deploy?.publicKey ?? hashPlaceholder} />
-                  </Link>
-                  <CopyToClipboard textToCopy={deploy?.publicKey ?? ''} />
-                </>,
-                isLoading,
-                { width: '60%' },
-              )}
-            </DetailDataValue>
-          </li>
-          <li>
-            <DetailDataLabel>{t('deploy-hash')}</DetailDataLabel>
-            <DetailDataValue height="2rem">
-              {withSkeletonLoading(
-                <>
-                  <Hash hash={deploy?.deployHash ?? hashPlaceholder} />
-                  <CopyToClipboard textToCopy={deploy?.deployHash ?? ''} />
-                </>,
-                isLoading,
-                { width: '60%' },
-              )}
-            </DetailDataValue>
-          </li>
-          <li>
-            <DetailDataLabel>{t('raw-data')}</DetailDataLabel>
-            <DetailDataValue>
-              {withSkeletonLoading(
-                deploy?.rawDeploy && <RawData rawData={deploy.rawDeploy} />,
-                isLoading,
-                { width: 200, height: '2.25rem' },
-              )}
-            </DetailDataValue>
-          </li>
-        </DetailDataList>
-      </DetailDataWrapper>
-    </InfoCard>
+      </HeaderContent>
+      <InfoCard>
+        <DetailDataWrapper>
+          <DetailDataList>
+            <li>
+              <DetailDataLabel>{t('block-hash')}</DetailDataLabel>
+              <DetailDataValue height="2rem">
+                {withSkeletonLoading(
+                  <>
+                    <Link
+                      to={`/block/${deploy?.blockHash ?? ''}`}
+                      style={{ color: '#2230f0' }}>
+                      <Hash hash={deploy?.blockHash ?? hashPlaceholder} />
+                    </Link>
+                    <CopyToClipboard textToCopy={deploy?.blockHash ?? ''} />
+                  </>,
+                  isLoading,
+                  { width: '60%' },
+                )}
+              </DetailDataValue>
+            </li>
+            <li>
+              <DetailDataLabel>{t('public-key')}</DetailDataLabel>
+              <DetailDataValue height="2rem">
+                {withSkeletonLoading(
+                  <>
+                    <Link
+                      to={`/account/${deploy?.publicKey ?? ''}`}
+                      style={{ color: '#2230f0' }}>
+                      <Hash hash={deploy?.publicKey ?? hashPlaceholder} />
+                    </Link>
+                    <CopyToClipboard textToCopy={deploy?.publicKey ?? ''} />
+                  </>,
+                  isLoading,
+                  { width: '60%' },
+                )}
+              </DetailDataValue>
+            </li>
+            <li>
+              <DetailDataLabel>{t('deploy-hash')}</DetailDataLabel>
+              <DetailDataValue height="2rem">
+                {withSkeletonLoading(
+                  <>
+                    <Hash hash={deploy?.deployHash ?? hashPlaceholder} />
+                    <CopyToClipboard textToCopy={deploy?.deployHash ?? ''} />
+                  </>,
+                  isLoading,
+                  { width: '60%' },
+                )}
+              </DetailDataValue>
+            </li>
+            <li>
+              <DetailDataLabel>{t('raw-data')}</DetailDataLabel>
+              <DetailDataValue>
+                {withSkeletonLoading(
+                  deploy?.rawDeploy && <RawData rawData={deploy.rawDeploy} />,
+                  isLoading,
+                  { width: 200, height: '2.25rem' },
+                )}
+              </DetailDataValue>
+            </li>
+          </DetailDataList>
+        </DetailDataWrapper>
+      </InfoCard>
+    </>
   );
 };
 
-const DeployHeading = styled(Heading)`
-  font-size: 1.125rem;
-  font-weight: ${fontWeight.medium};
-  margin-bottom: 1rem;
-`;
+const HeaderContent = styled.div``;
 
 const HashWrapper = styled.div`
   display: flex;
@@ -132,14 +133,17 @@ const HashWrapper = styled.div`
   min-height: ${pxToRem(75)};
 `;
 
-const HashHeading = styled(GradientHeading)<{
+const HashHeading = styled(Heading)<{
   isTruncated: boolean;
   isMobile: boolean;
 }>`
-  font-weight: ${fontWeight.extraBold};
+  font-weight: ${fontWeight.medium};
   display: inline;
   margin: 0;
   min-width: ${pxToRem(360)};
-  width: ${({ isTruncated }) => (isTruncated ? '10%' : '95%')};
+  width: ${({ isTruncated }) => (isTruncated ? '40%' : '75vw')};
   overflow-wrap: ${({ isMobile }) => (isMobile ? 'none' : 'break-word')};
+  font-size: ${pxToRem(60)};
+  color: #2230f0;
+  line-height: 65px;
 `;
