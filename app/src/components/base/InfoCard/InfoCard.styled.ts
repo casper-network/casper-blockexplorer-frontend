ce1276c2004c70f559dd359142999760954bff82
david <lostmorgan@gmail.com>
2023-05-05 14:20:39 -0400

    chore: rm dead code

--- app/src/components/base/InfoCard/InfoCard.styled.ts
+++ app/src/components/base/InfoCard/InfoCard.styled.ts
@@ -35,43 +35,6 @@ export const HeadContentWrapper = styled.div`
   margin-bottom: 2rem;
 `;
 
-export const FootContentWrapper = styled(Card.Footer)`
+export const FootContentWrapper = styled.div`
   margin-top: 1rem;
 `;
-
-// export const InfoCardSection = styled.section`
-//   margin: 0;
-//   padding: 0;
-//   width: 100%;
-
-//   @media (min-width: ${breakpoints.md}) {
-//     padding-top: ${pxToRem(24)};
-//   }
-
-//   @media only screen and (min-width: ${breakpoints.lg}) {
-//     padding-top: ${pxToRem(15)};
-//   }
-// `;
-
-// export const InfoCardContentWrapper = styled.div`
-//   width: 100%;
-//   background: ${props => props.theme.background.primary};
-//   border: 3px solid ${props => props.theme.border};
-//   box-shadow: 0px 0.125rem 0.5rem ${props => props.theme.boxShadow};
-//   border-radius: 0.35rem;
-//   padding: 2rem;
-//   overflow-x: auto;
-//   margin-bottom: 2rem;
-
-//   @media only screen and (min-width: ${breakpoints.md}) {
-//     max-width: 100vw;
-//   }
-// `;
-
-// export const HeadContentWrapper = styled.div`
-//   margin-bottom: 2rem;
-// `;
-
-// export const FootContentWrapper = styled.div`
-//   margin-top: 1rem;
-// `;
