4b4de4d039983ff147388b8342bcda67de57ab8f
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-05-19 10:05:46 -0700

    feat: add mockDeploy

--- app/src/components/cards/DeployDetailsCard/DeployDetailsCard.test.tsx
+++ app/src/components/cards/DeployDetailsCard/DeployDetailsCard.test.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import { DeployStatus } from 'src/api/types';
-import { render } from '../../../test-utils';
+import { getMockDeploy, render } from '../../../test-utils';
 import { DeployDetailsCard } from './DeployDetailsCard';
 
-const mockDeploy = {
-  timestamp: 0,
-  timeSince: '',
-  readableTimestamp: '',
-  deployHash: 'testDeployHash',
-  blockHash: 'testBlockHash',
-  publicKey: 'testPublicKey',
-  status: DeployStatus.Success,
-  amount: '',
-  action: '',
-  deployType: '',
-  paymentAmount: '',
-  cost: '',
-  rawDeploy: '',
-};
-
-jest.mock('react-i18next', () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: (str: string) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-      },
-    };
-  },
-}));
+const mockDeploy = getMockDeploy()
 
 describe('DeployDetailsCard', () => {
   it('should render DeployDetailsCard  ', () => {
