7f1066cfab4927e63347ab393e439c4efe4671e9
mgibson-casper <matthew@casperlabs.io>
2023-06-01 15:13:41 -0600

    feat-178: create util func for setting url search params

--- app/src/store/utils.ts
+++ app/src/store/utils.ts
@@ -17,3 +17,17 @@ export const setInitialStateWithLSTableOptions = <T>(
     tableOptions,
   };
 };
+
+export const setUrlSearchParams = (key: string, value: string) => {
+  if ('URLSearchParams' in window) {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+
+    const newRelativePathQuery = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    // eslint-disable-next-line no-restricted-globals
+    history.pushState(null, '', newRelativePathQuery);
+  }
+};
