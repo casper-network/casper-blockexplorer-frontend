8223b978f5f365284ab0a46ca09d50d62f25a3ff
DorianKwan <bryce.sayers-kwan@live.ca>
2023-06-05 11:49:30 +0100

    fix: block details e2e test

--- test/cypress/e2e/block.cy.ts
+++ test/cypress/e2e/block.cy.ts
@@ -1,42 +1,12 @@
-interface RpcRequestBody {
-  id: string;
-  jsonrpc: string;
-  method: string;
-  params?: any;
-}
-interface RpcResponseBody {
-  id: string;
-  jsonrpc: string;
-  result: any;
-}
-
 describe('Block Page', () => {
-  const middlewareUrl = Cypress.env('MIDDLEWARE_URL') as string;
-
-  it('can visit at /block/:blockHeight?type=height', () => {
+  it('can visit at /block/:blockHeight', () => {
     const blockHeight = 1;
-    cy.intercept('POST', `${middlewareUrl}/rpc`, req => {
-      const body = req.body as RpcRequestBody;
-      if (
-        body.method === 'chain_get_block' &&
-        body.params &&
-        body.params.block_identifier
-      ) {
-        req.alias = 'blockFetch';
-      }
-    });
 
     cy.visit(`/block/${blockHeight}?type=height`);
 
-    cy.wait('@blockFetch').its('response.statusCode').should('equal', 200);
+    cy.contains('Block Height').should('be.visible');
 
-    cy.wait('@blockFetch').then(interception => {
-      const { result } = interception.response.body as RpcResponseBody;
-      const blockHash = result.block.hash as string;
-      const truncatedBlockHash = `${blockHash.slice(0, 5)}...${blockHash.slice(
-        -5,
-      )}`;
-      cy.contains(truncatedBlockHash).should('be.visible');
-    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    cy.getByData('block-height').should('be.visible').contains(blockHeight);
   });
 });
