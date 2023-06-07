a21b26f0353e9a019b027453fb8a1d651c752753
mgibson-casper <matthew@casperlabs.io>
2023-06-07 09:51:12 -0600

    feat-371: define constants as consts

--- app/src/store/constants.ts
+++ app/src/store/constants.ts
@@ -2,5 +2,5 @@ export const VALIDATOR_TABLE_OPTIONS = 'validatorTableOptions';
 export const PEER_TABLE_OPTIONS = 'peerTableOptions';
 export const BLOCK_TABLE_OPTIONS = 'blockTableOptions';
 
-export const TABLE_OPTIONS_PAGE_SIZE_PRESETS = [5, 10, 20];
-export const TABLE_OPTIONS_ORDERING_PRESETS = ['desc', 'asc'];
+export const TABLE_OPTIONS_PAGE_SIZE_PRESETS = [5, 10, 20] as const;
+export const TABLE_OPTIONS_ORDERING_PRESETS = ['desc', 'asc'] as const;
