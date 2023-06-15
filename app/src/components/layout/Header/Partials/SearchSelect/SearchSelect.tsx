abf2b274fcbbd854819ff7ac75762f1e3fdc2669
david <lostmorgan@gmail.com>
2023-06-15 16:31:19 -0400

    chore: rm dead code

--- app/src/components/layout/Header/Partials/SearchSelect/SearchSelect.tsx
+++ app/src/components/layout/Header/Partials/SearchSelect/SearchSelect.tsx
@@ -67,7 +67,6 @@ export const SearchSelect: React.FC<SearchSelectProps> = ({
                 return (
                   <li key={option.value} data-testid={`button-${index + 1}`}>
                     <MobileSelectButton
-                      bgColor=""
                       onClick={handleClick}
                       style={{
                         backgroundColor:
