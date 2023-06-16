4cdbd89fa07af55ffe5e3ab8c6b75ee571bc7317
mgibson-casper <matthew@casperlabs.io>
2023-06-16 09:58:41 -0600

    feat-393: setup some basic table logic for deploys

--- app/src/components/tables/DeploysTable/DeploysTable.tsx
+++ app/src/components/tables/DeploysTable/DeploysTable.tsx
@@ -1,12 +1,47 @@
-import React, { useEffect } from 'react';
-import { fetchDeploys, useAppDispatch } from 'src/store';
+import { ColumnDef } from '@tanstack/react-table';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ApiData } from 'src/api/types';
+import {
+  Loading,
+  fetchDeploys,
+  getDeploys,
+  getDeploysLoadingStatus,
+  useAppDispatch,
+  useAppSelector,
+} from 'src/store';
 
 export const DeploysTable: React.FC = () => {
+  const { t } = useTranslation();
+
+  const [isTableLoading, setIsTableLoading] = useState(false);
+
   const dispatch = useAppDispatch();
+
+  const deploys = useAppSelector(getDeploys);
+  const deploysLoadingStatus = useAppSelector(getDeploysLoadingStatus);
+
+  const isLoadingPage =
+    deploysLoadingStatus !== Loading.Complete && !deploys.length;
 
   useEffect(() => {
     dispatch(fetchDeploys());
   }, [dispatch]);
+
+  const header = useMemo(() => <div>header placeholder</div>, []);
+  const footer = useMemo(() => <div>footer placeholder</div>, []);
+
+  const columns = useMemo<ColumnDef<ApiData.SidecarDeploy>[]>(
+    () => [
+      {
+        header: `${t('deploy-hash')}`,
+        id: 'deploy_hash',
+        accessorKey: 'deploy_hash',
+        cell: ({ getValue }) => getValue<string>(),
+      },
+    ],
+    [],
+  );
 
   // TODO: ticket #393
   return <>deploys table placeholder</>;
