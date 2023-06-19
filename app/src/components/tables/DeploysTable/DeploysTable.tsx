9f99ee2a0f9d9842528bf290d42a50cf21788e54
mgibson-casper <matthew@casperlabs.io>
2023-06-19 11:01:45 -0600

    feat-394: add hashes for all links

--- app/src/components/tables/DeploysTable/DeploysTable.tsx
+++ app/src/components/tables/DeploysTable/DeploysTable.tsx
@@ -1,8 +1,11 @@
+import styled from '@emotion/styled';
 import { ColumnDef } from '@tanstack/react-table';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { ApiData } from 'src/api/types';
 import { Table } from 'src/components/base';
+import { StyledCopyToClipboard } from 'src/components/utility';
 import {
   Loading,
   fetchDeploys,
@@ -11,6 +14,7 @@ import {
   useAppDispatch,
   useAppSelector,
 } from 'src/store';
+import { truncateHash } from 'src/utils';
 
 export const DeploysTable: React.FC = () => {
   const { t } = useTranslation();
@@ -21,6 +25,8 @@ export const DeploysTable: React.FC = () => {
 
   const deploys = useAppSelector(getDeploys);
   const deploysLoadingStatus = useAppSelector(getDeploysLoadingStatus);
+
+  console.log({ deploys });
 
   const isLoadingPage =
     deploysLoadingStatus !== Loading.Complete && !deploys.length;
@@ -45,14 +51,51 @@ export const DeploysTable: React.FC = () => {
         header: `${t('deploy-hash')}`,
         id: 'deploy_hash',
         accessorKey: 'deploy_hash',
-        cell: ({ getValue }) => getValue<string>(),
+        cell: ({ getValue }) => (
+          <HashAndCopyToClipboardWrapper>
+            <StyledHashLink
+              to={{
+                pathname: `/deploy/${getValue<string>()}`,
+              }}>
+              {truncateHash(getValue<string>())}
+            </StyledHashLink>
+            <StyledCopyToClipboard textToCopy={getValue<string>()} />
+          </HashAndCopyToClipboardWrapper>
+        ),
         enableSorting: false,
       },
       {
         header: `${t('block-hash')}`,
         id: 'block_hash',
         accessorKey: 'deploy_processed.block_hash',
-        cell: ({ getValue }) => getValue<string>(),
+        cell: ({ getValue }) => (
+          <HashAndCopyToClipboardWrapper>
+            <StyledHashLink
+              to={{
+                pathname: `/block/${getValue<string>()}`,
+              }}>
+              {truncateHash(getValue<string>())}
+            </StyledHashLink>
+            <StyledCopyToClipboard textToCopy={getValue<string>()} />
+          </HashAndCopyToClipboardWrapper>
+        ),
+        enableSorting: false,
+      },
+      {
+        header: `${t('public-key')}`,
+        id: 'public_key',
+        accessorKey: 'deploy_processed.account',
+        cell: ({ getValue }) => (
+          <HashAndCopyToClipboardWrapper>
+            <StyledHashLink
+              to={{
+                pathname: `/account/${getValue<string>()}`,
+              }}>
+              {truncateHash(getValue<string>())}
+            </StyledHashLink>
+            <StyledCopyToClipboard textToCopy={getValue<string>()} />
+          </HashAndCopyToClipboardWrapper>
+        ),
         enableSorting: false,
       },
     ],
@@ -60,14 +103,23 @@ export const DeploysTable: React.FC = () => {
   );
 
   return (
-    <Table
+    <Table<ApiData.SidecarDeploy>
       header={header}
       columns={columns}
       data={deploys}
       footer={footer}
-      tableBodyLoading={isTableLoading}
+      tableBodyLoading={isTableLoading || isLoadingPage}
       isLastPage={false}
       currentPageSize={10}
+      placeholderData={{}}
     />
   );
 };
+
+const HashAndCopyToClipboardWrapper = styled.div`
+  white-space: nowrap;
+`;
+
+const StyledHashLink = styled(Link)`
+  color: ${props => props.theme.text.hash};
+`;
