2f84342216fb70dcba658927e5f9f16c034f81e8
david <lostmorgan@gmail.com>
2023-07-07 19:09:07 -0400

    chore: rm test objects

--- app/src/mocks/mock-deploys-table.tsx
+++ app/src/mocks/mock-deploys-table.tsx
@@ -28,44 +28,6 @@ export const getMockProcessedSidecarDeploy = () => [
     },
     csprToUsdConversion: 10,
   },
-  {
-    deployHash:
-      '4b0fddb3ed65ddf076892dddbcb98694921e74ea90d33137121a58985859ddcf',
-    blockHash:
-      '92d9b84db79132a77f76216c7d81b2243fe92ef26db885ae0d64ee585e4799fa',
-    publicKey:
-      '0202ed20f3a93b5386bc41b6945722b2bd4250c48f5fa0632adf546e2f3ff6f4ddee',
-    timestamp: '2023-06-15T22:13:16.579Z',
-    contractType: 'Transfer',
-    amount: {
-      motes: '505124902204510',
-      usd: '5051249902',
-    },
-    cost: {
-      motes: '100000000',
-      usd: '10000000',
-    },
-    csprToUsdConversion: 10,
-  },
-  {
-    deployHash:
-      '4b0fddb3ed65ddf076892dddbcb98694921e74ea90d33137121a58985859ddcf',
-    blockHash:
-      '92d9b84db79132a77f76216c7d81b2243fe92ef26db885ae0d64ee585e4799fa',
-    publicKey:
-      '0202ed20f3a93b5386bc41b6945722b2bd4250c48f5fa0632adf546e2f3ff6f4ddee',
-    timestamp: '2023-06-15T22:13:16.579Z',
-    contractType: 'Transfer',
-    amount: {
-      motes: '505124902204510',
-      usd: '5051249902',
-    },
-    cost: {
-      motes: '100000000',
-      usd: '10000000',
-    },
-    csprToUsdConversion: 10,
-  },
 ];
 
 const mockProcessedSidecarDeploys = getMockProcessedSidecarDeploy();
